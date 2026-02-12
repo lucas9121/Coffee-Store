826d116f365ad02986bafce7a38a3bf642ea250d
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-12 18:26:03 -0500

    adding backend information

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const cors = require("cors");
+const connectDB = require("./config/db")
 // const orderRoutes = require("./routes/orderRoutes");
 // const kioskRoutes = require("./routes/kioskRoutes");
 
 require("dotenv").config();
 
 const app = express();
+
+// Middleware
+app.use(cors())
 app.use(express.json());
 
-//Routes
+// Connect to DB
+connectDB();
+
+// Routes
 // app.use("/orders", orderRoutes);
 // app.use("/kiosk", kioskRoutes);
 
 
 const PORT = process.env.PORT || 3002;
 
-app.listen(prototype, function(){
+app.listen(PORT, function(){
   console.log("Backend running on port: ", PORT)
 })
