899445898c86fc0efa0152df86e53185e4481241
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-12 20:31:32 -0500

    getOrder function test

--- backend/tests/orderController.test.js
+++ backend/tests/orderController.test.js
@@ -1,7 +1,10 @@
-// controllers/orderController.test.js
+
+beforeEach(() => {
+  jest.clearAllMocks();
+})
 
 // Import the controller function we want to test
-const { createOrder } = require("../controllers/orderController");
+const { createOrder, getOrder } = require("../controllers/orderController");
 
 // Import the Order model
 const Order = require("../models/Order");
@@ -9,7 +12,7 @@ const Order = require("../models/Order");
 // Replace the real Order model with a mocked version
 jest.mock("../models/Order");
 
-describe("Order Controller", () => {
+describe("createOrder", () => {
 
   it("should create an order successfully", async () => {
 
@@ -36,3 +39,56 @@ describe("Order Controller", () => {
   });
 
 });
+
+describe("getOrder", () => {
+  // Test 1 - Successfull fetch
+  it("should get order successfully", async () => {
+    // Fake request
+    const req = {params: {id: "123"}};
+    const fakeOrder = {_id: "123", customerName: "Alice"};
+
+    // Fake response
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    // Mock
+    Order.findById.mockResolvedValue(fakeOrder);
+
+    // Call controller function
+    await getOrder(req, res);
+
+    // Check if status was called
+    expect(res.status).toHaveBeenCalledWith(200);
+
+    // Check if json was called with the correct data
+    expect(res.json).toHaveBeenCalledWith(fakeOrder);
+  });
+
+  // Test 2 - Order not found
+  it("should not find the order", async () => {
+    const req = {params: {id: "123"}};
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    Order.findById.mockResolvedValue(null);
+    await getOrder(req, res);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({message: "Order not found"})
+  });
+
+  // Test 3 - Invalid ID error
+  it("should have invalid id", async () => {
+    const req = {params: {id: "123"}};
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    Order.findById.mockRejectedValue(new Error("Invalid ID"));
+    await getOrder(req, res);
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({message: "Invalid ID"})
+  });
+})
