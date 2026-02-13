49b5339e5062cf5329621cf8b446e473ea6d20f1
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-13 09:14:00 -0500

    updating schema

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  customerName: {type: String, required: true},
-  status: {
-    type: String, 
-    enum: ["PLACED", "IN PROGRESS", "READY", "COMPLETED", "CANCELLED"], 
-    default: "PLACED"
+const orderSchema = new mongoose.Schema(
+  {
+    customerName: {type: String, required: true},
+    status: {
+      type: String, 
+      enum: ["PLACED", "IN PROGRESS", "READY", "COMPLETED", "CANCELLED"], 
+      default: "PLACED"
+    },
+    orderItems: [{type: mongoose.Schema.Types.ObjectId, ref: "OrderItem"}]
   },
-  createdAt: {type: Date, default: Date.now}
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Order', orderSchema);
