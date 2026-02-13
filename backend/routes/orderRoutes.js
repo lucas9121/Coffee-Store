0d63ab4dee06827779ae1426b31553fcddb7603f
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-13 15:33:17 -0500

    adding patch route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {createOrder, getOrder} = require("../controllers/orderController");
+const {createOrder, getOrder, updateOrderStatus} = require("../controllers/orderController");
 
 
 router.post('/', createOrder);
 router.get('/:id', getOrder);
+router.patch('/:id/status', updateOrderStatus)
 
 module.exports = router;
