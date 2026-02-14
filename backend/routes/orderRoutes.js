f2d7fc0d2a7a4e4fbdd7b086da38596f887ffb61
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-14 12:33:27 -0500

    adding delete order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {createOrder, getOrder, updateOrderStatus} = require("../controllers/orderController");
+const {createOrder, getOrder, updateOrderStatus, deleteOrder} = require("../controllers/orderController");
 
 
 router.post('/', createOrder);
 router.get('/:id', getOrder);
 router.patch('/:id/status', updateOrderStatus)
+router.delete('/:id', deleteOrder)
 
 module.exports = router;
