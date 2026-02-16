e473f253d465f5bf01f73d85e1507885d34c4408
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-16 00:05:45 -0500

    adding getAllOrderItems route

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {getOrderItem} = require("../controllers/orderItemController")
+const {getOrderItem, getAllOrderItems} = require("../controllers/orderItemController")
 
+router.get("/", getAllOrderItems);
 router.get("/:id", getOrderItem);
 
 module.exports = router
