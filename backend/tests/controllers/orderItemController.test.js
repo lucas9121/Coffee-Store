33825729b91569fd060af834bf4ef153f48474eb
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-16 00:04:40 -0500

    adding getAllOrderItems controller test

--- backend/tests/controllers/orderItemController.test.js
+++ backend/tests/controllers/orderItemController.test.js
@@ -1,4 +1,4 @@
-const {getOrderItem} = require("../../controllers/orderItemController");
+const {getOrderItem, getAllOrderItems} = require("../../controllers/orderItemController");
 const OrderItem = require("../../models/OrderItem");
 
 jest.mock("../../models/OrderItem");
@@ -56,5 +56,42 @@ describe("getOrderItem", () => {
     await getOrderItem(req, res);
     expect(res.status).toHaveBeenCalledWith(400)
     expect(res.json).toHaveBeenCalledWith({message: "Invalid ID"})
+  });
+})
+
+describe("getAllOrderItems", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  
+  it("should get all orderItems successfully", async () => {
+    const req = {};
+    const fakeOrderItems = [
+      {_id: "507f191e810c19729de860ea", name: "Latte", price: 5},
+      {_id: "507f191e810c19729de860eb", name: "Cappuccino", price: 6},
+      {_id: "507f191e810c19729de860ec", name: "Espresso", price: 2},
+    ];
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    OrderItem.find.mockResolvedValue(fakeOrderItems);
+    await getAllOrderItems(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(fakeOrderItems);
+  });
+
+  // Test 2 - Not found error
+  it("should have invalid id", async () => {
+    const req = {};
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+    const mockError = new Error("Server error");
+    OrderItem.find.mockRejectedValue(mockError);
+    await getAllOrderItems(req, res);
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({message: mockError.message})
   });
 })
