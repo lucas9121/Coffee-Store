afded9bbfe08540629cb316f210942927ad9d4d0
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-16 00:05:28 -0500

    updating getAllOrderItems function

--- backend/controllers/orderItemController.js
+++ backend/controllers/orderItemController.js
@@ -11,7 +11,7 @@ async function getAllOrderItems(req, res) {
     const orderItems = await OrderItem.find({});
     res.status(200).json(orderItems);
   } catch (error) {
-    res.status(400).json({message: error.message})
+    res.status(500).json({message: "Server error"})
   }
 }
 
