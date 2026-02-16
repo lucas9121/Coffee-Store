8e6cc6499b09fde9c909271621ef1c235ba1a2ec
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-15 23:46:14 -0500

    changing getOrder Item route to menu route

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {getOrderItem} = require("../controllers/orderItemController")
 
-router.get("/:id", getOrderItem)
+router.get("/:id", getOrderItem);
 
 module.exports = router
--- backend/tests/routes/orderItemRoutes.test.js
+++ backend/tests/routes/orderItemRoutes.test.js
@@ -4,20 +4,20 @@ const app = require("../../app");
 const OrderItem = require("../../models/OrderItem");
 jest.mock("../../models/OrderItem");
 
-const orderItemRoutes = require("../../routes/orderItemRoutes");
-app.use("/orders", orderItemRoutes)
+const menuRoutes = require("../../routes/menuRoutes");
+app.use("/menu", menuRoutes)
 
 describe("Order Item Routes (mocked DB)", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-
-  // GET /orders/:id
-  it("should return order by id", async () => {
+  
+  // GET /menu/:id
+  it("should return order item by id", async () => {
       const validId = "507f191e810c19729de860ea";
       const fakeOrderItem = {_id: `${validId}`, name: "Latte", price: 5};
       OrderItem.findById.mockResolvedValue(fakeOrderItem);
-      const response = await request(app).get(`/orders/${validId}`);
+      const response = await request(app).get(`/menu/${validId}`);
   
       expect(OrderItem.findById).toHaveBeenCalledWith(`${validId}`);
       expect(response.status).toBe(200);
