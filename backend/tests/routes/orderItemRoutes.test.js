2d1f216f63f7b2fc125d57eb83208c4f8640be42
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-16 00:06:36 -0500

    adding getAllOrderItems route test

--- backend/tests/routes/orderItemRoutes.test.js
+++ backend/tests/routes/orderItemRoutes.test.js
@@ -11,7 +11,22 @@ describe("Order Item Routes (mocked DB)", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
+
+  // GET /menu
+  it("should return all order items", async () => {
+      const fakeOrderItems = [
+      {_id: "507f191e810c19729de860ea", name: "Latte", price: 5},
+      {_id: "507f191e810c19729de860eb", name: "Cappuccino", price: 6},
+      {_id: "507f191e810c19729de860ec", name: "Espresso", price: 2},
+    ];
+      OrderItem.find.mockResolvedValue(fakeOrderItems);
+      const response = await request(app).get(`/menu`);
   
+      expect(OrderItem.find).toHaveBeenCalledWith({});
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(fakeOrderItems);
+  });
+
   // GET /menu/:id
   it("should return order item by id", async () => {
       const validId = "507f191e810c19729de860ea";
