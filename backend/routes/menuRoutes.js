c17d7791b2a5cc495efabfaf7b0862f848d06415
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-17 01:12:09 -0500

    adding deleteOrderItem delete route

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getOrderItem, getAllOrderItems, createItem, updateOrderItem} = require("../controllers/orderItemController")
+const {getOrderItem, getAllOrderItems, createItem, updateOrderItem, deleteOrderItem} = require("../controllers/orderItemController")
 
 router.post("/", createItem);
 router.get("/", getAllOrderItems);
 router.get("/:id", getOrderItem);
 router.patch("/:id", updateOrderItem);
+router.delete("/:id", deleteOrderItem);
 
 module.exports = router
