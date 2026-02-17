7a76824f422415dcb1c9ae0e996eab4e21458eb6
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-16 22:19:34 -0500

    adding updateOrderItem patch route

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {getOrderItem, getAllOrderItems, createItem} = require("../controllers/orderItemController")
+const {getOrderItem, getAllOrderItems, createItem, updateOrderItem} = require("../controllers/orderItemController")
 
 router.post("/", createItem);
 router.get("/", getAllOrderItems);
 router.get("/:id", getOrderItem);
+router.patch("/:id", updateOrderItem);
 
 module.exports = router
