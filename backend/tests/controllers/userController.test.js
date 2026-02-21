ac7e04ee1d41cab6eb740cf79ef8b13609a70811
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-21 14:47:39 -0500

    adding loginUser test

--- backend/tests/controllers/userController.test.js
+++ backend/tests/controllers/userController.test.js
@@ -1,8 +1,12 @@
-const {createUser} = require("../../controllers/userController");
+const {createUser, loginUser} = require("../../controllers/userController");
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 
 jest.mock("../../models/User");
+jest.mock("bcrypt", () => ({
+  compare: jest.fn()
+}));
 jest.mock("jsonwebtoken", () => ({
   sign: jest.fn()
 }));
@@ -112,4 +116,133 @@ describe("createUser", () => {
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({message: unknownError.message})
   })
+})
+
+describe("loginUser", () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  });
+
+  // Test 1 - login successfull
+  it("should return token and user after successfull login", async () => {
+    const req = {
+      body: {
+        email: "  ALICE@email.com   ",
+        password: "password",
+      }}
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    const dbUser = {
+      _id: "507f191e810c19729de860ea",
+      name: "Alice",
+      email: "alice@email.com",
+      password: "$2b$06$fakehash",
+      account: "user",
+      securityQuestions: [
+        {question: "What was your first car?"},
+        {question: "What is the name of your first pet?"}
+      ]
+    }
+
+    User.findOne.mockResolvedValue(dbUser);
+    bcrypt.compare.mockResolvedValue(true);
+    jwt.sign.mockReturnValue("fake-token");
+
+    await loginUser(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(User.findOne).toHaveBeenCalledWith({email: "alice@email.com"});
+    expect(User.findOne).not.toHaveBeenCalledWith({email: "  ALICE@email.com   "});
+    expect(bcrypt.compare).toHaveBeenCalledWith("password", dbUser.password);
+    expect(jwt.sign).toHaveBeenCalledWith(
+      { userId: dbUser._id, account: dbUser.account},
+      process.env.SECRET,
+      {expiresIn: "24h"}
+    );
+    expect(res.json).toHaveBeenCalledWith({token: "fake-token", user: dbUser});
+  });
+
+  // Test 2 - Missing credentials
+  const missingCases = [
+    { label: "email", body: { password: "password" } },
+    { label: "password", body: { email: "alice@email.com" } },
+  ];
+
+  missingCases.forEach(({label, body}) => {
+    it(`should return 400 for missin ${label}`, async () => {
+      const req = {body}
+
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+      };
+
+      await loginUser(req, res)
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({message: "Missing Credentials"})
+      expect(User.findOne).not.toHaveBeenCalled();
+      expect(bcrypt.compare).not.toHaveBeenCalled();
+      expect(jwt.sign).not.toHaveBeenCalled();
+    });
+  });
+
+  // Test 3 - User not found
+  it("should return 400 if user not found", async () => {
+    const req = {
+      body: {
+        email: "alice@email.com",
+        password: "password",
+      }
+    };
+    
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    User.findOne.mockResolvedValue(null);
+
+    await loginUser(req, res)
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({message: "Bad Credentials"});
+    expect(bcrypt.compare).not.toHaveBeenCalled()
+    expect(jwt.sign).not.toHaveBeenCalled();
+  })
+
+  // Test 4 - Password mismatch
+    it("should return 400 if password is incorrect", async () => {
+    const req = {
+      body: {
+        email: "alice@email.com",
+        password: "password",
+      }
+    };
+    
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    const dbUser = {
+      _id: "1",
+      email: "alice@email.com",
+      password: "hash",
+      account: "user"
+    };
+
+    User.findOne.mockResolvedValue(dbUser);
+    bcrypt.compare.mockResolvedValue(false);
+
+    await loginUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({message: "Bad Credentials"});
+    expect(jwt.sign).not.toHaveBeenCalled();
+  });
 })
