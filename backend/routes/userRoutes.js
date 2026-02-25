6165ecb90f1b0493cbcd193de45a5e098f3979fc
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-25 13:48:40 -0500

    adding user routes with auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const requireAuth = require("../middleware/requireAuth")
 const {
   createUser,
   loginUser,
   getCurrentUser,
   updateUserPassword,
   updateUserProfile,
-  toggleFavorites,
   updateUserSecurityQuestion,
+  toggleFavorites,
   deleteUser 
 } = require("../controllers/userController");
 
+router.post("/", createUser);
+router.post("/login", loginUser);
+
+router.use("/me", requireAuth);
+
+router.get("/me", getCurrentUser);
+router.patch("/me/password", updateUserPassword);
+router.patch("/me/profile", updateUserProfile);
+router.patch("/me/security-question", updateUserSecurityQuestion);
+router.patch("/me/favorites/:orderItemId", toggleFavorites);
+router.delete("/me", deleteUser);
 
 
 module.exports = router
