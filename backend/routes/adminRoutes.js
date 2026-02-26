25471cdc6dcbbf3d2144e72f29f4b2873b15cf8d
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-26 18:55:40 -0500

    adding admin routes

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const requireAuth = require("../middleware/requireAuth");
+const authorizeRoles = require("../middleware/authorizeRoles");
+const {updateUserAccount, getAllUsers} = require("../controllers/adminController");
+
+router.use(requireAuth);
+router.use(authorizeRoles("admin"))
+
+router.get("/users", getAllUsers);
+router.patch("/users/:userId/account", updateUserAccount);
+
+moduler.exports = router
