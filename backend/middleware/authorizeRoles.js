d7cf6ff4c50e3f5e21fddc08651ae16a02f1ba30
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-26 11:34:56 -0500

    adding authorizeRoles middleware

---
+++ backend/middleware/authorizeRoles.js
@@ -0,0 +1,12 @@
+
+function authorizeRoles(allowedRoles = []) {
+  return (req, res, next) => {
+    if(!req.user) return res.status(401).json({message: "Unauthorized"});
+    if(!allowedRoles.includes(req.user.account)){
+      return res.status(403).json({message: "Forbidden"});
+    }
+    next();
+  };
+};
+
+module.exports = authorizeRoles;
