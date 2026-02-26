6803ab3baf2240e9c8de8889c4325819e6df7a6b
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-26 12:41:56 -0500

    updating authorizeRoles

--- backend/middleware/authorizeRoles.js
+++ backend/middleware/authorizeRoles.js
@@ -1,8 +1,10 @@
 
 function authorizeRoles(allowedRoles = []) {
+  const roles = Array.isArray(allowedRoles) ? allowedRoles : [allowedRoles];
+
   return (req, res, next) => {
     if(!req.user) return res.status(401).json({message: "Unauthorized"});
-    if(!allowedRoles.includes(req.user.account)){
+    if(!roles.includes(req.user.account)){
       return res.status(403).json({message: "Forbidden"});
     }
     next();
