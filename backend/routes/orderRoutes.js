fb95c09b62bf71d0a393c17d10b3352a2da6e051
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-02-26 20:31:54 -0500

    adding auth

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const requireAuth = require("../middleware/requireAuth");
+const authorizeRoles = require("../middleware/authorizeRoles")
 const {createOrder, getOrder, updateOrderStatus, deleteOrder} = require("../controllers/orderController");
 
 
 router.post('/', createOrder);
 router.get('/:id', getOrder);
-router.patch('/:id/status', updateOrderStatus)
-router.delete('/:id', deleteOrder)
+router.patch('/:id/status', requireAuth, authorizeRoles(["worker", "admin"]), updateOrderStatus)
+router.delete('/:id', requireAuth, authorizeRoles(["worker", "admin"]), deleteOrder)
 
 module.exports = router;
