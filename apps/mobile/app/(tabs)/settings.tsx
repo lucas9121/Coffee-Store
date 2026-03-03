5d6f5433334c38b434315b7b42f128862602858c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-03 13:07:10 -0500

    adding theme buttons

--- apps/mobile/app/(tabs)/settings.tsx
+++ apps/mobile/app/(tabs)/settings.tsx
@@ -1,8 +1,12 @@
 import { ThemedView } from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
 import { useAuth } from "@/context/AuthContext";
+import { useThemeMode } from "@/context/ThemeContext";
+
+
 
 export default function SettingsScreen() {
+  const { themeMode, setThemeMode } = useThemeMode();
   const {accountType, setAccountType} = useAuth();
 
   return(
@@ -12,6 +16,11 @@ export default function SettingsScreen() {
       <ThemedText onPress={() => setAccountType("guest")} >Set Guest</ThemedText>
       <ThemedText onPress={() => setAccountType("user")} >Set User</ThemedText>
       <ThemedText onPress={() => setAccountType("worker")} >Set Worker</ThemedText>
+      
+      <ThemedText>themeMode: {themeMode}</ThemedText>
+      <ThemedText onPress={() => setThemeMode("system")}>Theme: System</ThemedText>
+      <ThemedText onPress={() => setThemeMode("light")}>Theme: Light</ThemedText>
+      <ThemedText onPress={() => setThemeMode("dark")}>Theme: Dark</ThemedText>
     </ThemedView>
   )
 }
