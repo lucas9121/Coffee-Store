b58a5eaac5a4b1687a3b36add648c979aee79ef4
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-03 12:42:05 -0500

    updating color scheme

--- apps/mobile/hooks/use-color-scheme.ts
+++ apps/mobile/hooks/use-color-scheme.ts
@@ -1,1 +1,12 @@
-export { useColorScheme } from 'react-native';
+import {useColorScheme as useRNColorScheme} from "react-native";
+import { useThemeMode } from "@/context/ThemeContext";
+
+export function useColorScheme() {
+  const phoneScheme = useRNColorScheme() ?? "light";
+  const {themeMode} = useThemeMode();
+
+  if(themeMode === "light") return "light";
+  if(themeMode === "dark") return "dark";
+
+  return phoneScheme; //system
+}
