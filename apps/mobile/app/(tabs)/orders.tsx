ae6e82ffb164dd2b33d32a82c69acf866392f2ba
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-03 12:21:45 -0500

    using template style

--- apps/mobile/app/(tabs)/orders.tsx
+++ apps/mobile/app/(tabs)/orders.tsx
@@ -1,13 +1,14 @@
-import {View, Text} from "react-native";
-import { useAuth } from "../../context/AuthContext";
+import { ThemedView } from "@/components/themed-view";
+import { ThemedText } from "@/components/themed-text";
+import { useAuth } from "@/context/AuthContext";
 
 export default function OrdersScreen() {
   const {accountType} = useAuth();
 
   return(
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>{accountType === "worker" ? "Worker Orders" : "Order Menu"}</Text>
-      <Text>accountType: {accountType}</Text>
-    </View>
+    <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <ThemedText>{accountType === "worker" ? "Worker Orders" : "Order Menu"}</ThemedText>
+      <ThemedText>accountType: {accountType}</ThemedText>
+    </ThemedView>
   )
 }
