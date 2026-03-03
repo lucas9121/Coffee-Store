c0b416c61590b08e635a6e93454c42b06a30a864
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-03 12:18:44 -0500

    using template style

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,13 +1,14 @@
-import {View, Text} from "react-native";
+import { ThemedView } from "@/components/themed-view";
+import { ThemedText } from "@/components/themed-text";
 import {useAuth} from "../../context/AuthContext";
 
 export default function HomeScreen() {
   const {accountType} = useAuth();
 
   return(
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Home Screen</Text>
-      <Text>accountType: {accountType}</Text>
-    </View>
+    <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+      <ThemedText>Home Screen</ThemedText>
+      <ThemedText>accountType: {accountType}</ThemedText>
+    </ThemedView>
   )
 }
