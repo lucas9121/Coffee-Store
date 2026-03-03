d9aa886af701cacd6af4d4a1814708ec25599c58
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-03 11:25:17 -0500

    moving Auth

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import {AuthProvider} from "./context/AuthContext" 
+import {AuthProvider} from "../context/AuthContext" 
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
