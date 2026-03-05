0421b598ad75ea609b43ea80bc9515876582c2da
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-04 21:42:17 -0500

    adding token value

--- apps/mobile/context/AuthContext.tsx
+++ apps/mobile/context/AuthContext.tsx
@@ -2,17 +2,36 @@ import React, {createContext, useContext, useMemo, useState} from "react";
 
 type AccountType = "guest" | "user" | "worker";
 
+
 type AuthContextValue = {
   accountType: AccountType;
   setAccountType: React.Dispatch<React.SetStateAction<AccountType>>;
+
+  accessToken: string | null;
+  setAccessToken: React.Dispatch<React.SetStateAction<string | null>>;
+  
+  isInitializing: boolean;
+  setIsInitializing: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const AuthContext = createContext<AuthContextValue | null>(null);
 
 export function AuthProvider({children} : {children: React.ReactNode}){
   const [accountType, setAccountType] = useState<AccountType>("guest");
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [isInitializing, setIsInitializing] = useState<boolean>(true)
 
-  const value = useMemo(() => ({accountType, setAccountType}), [accountType]);
+  const value = useMemo(
+    () => ({
+      accountType, 
+      setAccountType,
+      accessToken,
+      setAccessToken,
+      isInitializing,
+      setIsInitializing
+    }), 
+    [accountType, accessToken, isInitializing]
+  );
 
   return( 
     <AuthContext.Provider value={value}>
