0989f6f5f8bb3b2aa6911e7a0ab3cb096cdcd297
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-07 14:28:34 -0500

    adding login screen

---
+++ apps/mobile/app/auth/login.tsx
@@ -0,0 +1,10 @@
+import {ThemedView} from "@/components/themed-view";
+import { ThemedText } from "@/components/themed-text";
+
+export default function LoginScreen() {
+  return(
+    <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}} >
+      <ThemedText>Login Screen</ThemedText>
+    </ThemedView>
+  )
+}
