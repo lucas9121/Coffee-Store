a838c8ab425206bcf6490ab9c851f1975aeedb01
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-10 12:49:31 -0400

    fixing name

--- apps/mobile/app/(tabs)/settings.tsx
+++ apps/mobile/app/(tabs)/settings.tsx
@@ -15,7 +15,7 @@ export default function SettingsScreen() {
   return(
     <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
       <ThemedText>Settings Screen</ThemedText>
-      <Button onPress={() => router.push("/(auth)/login")}>Log In</Button>
+      <Button onPress={() => router.push("./(auth)/login")}>Log In</Button>
       <ThemedText>accountType: {accountType}</ThemedText>
       <ThemedText onPress={() => setAccountType("guest")} >Set Guest</ThemedText>
       <ThemedText onPress={() => setAccountType("user")} >Set User</ThemedText>
