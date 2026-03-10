f67b581b378ba744fa17ff0e4a4f8d4a710e8e6a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-10 13:56:36 -0400

    updating settings tab

--- apps/mobile/app/(tabs)/settings.tsx
+++ apps/mobile/app/(tabs)/settings.tsx
@@ -9,22 +9,35 @@ import { useRouter } from "expo-router";
 
 export default function SettingsScreen() {
   const { themeMode, setThemeMode } = useThemeMode();
-  const {accountType, setAccountType} = useAuth();
+  const {accountType, logout} = useAuth();
   const router = useRouter();
-
-  return(
-    <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <ThemedText>Settings Screen</ThemedText>
-      <Button onPress={() => router.push("./(auth)/login")}>Log In</Button>
-      <ThemedText>accountType: {accountType}</ThemedText>
-      <ThemedText onPress={() => setAccountType("guest")} >Set Guest</ThemedText>
-      <ThemedText onPress={() => setAccountType("user")} >Set User</ThemedText>
-      <ThemedText onPress={() => setAccountType("worker")} >Set Worker</ThemedText>
-      
-      <ThemedText>themeMode: {themeMode}</ThemedText>
-      <ThemedText onPress={() => setThemeMode("system")}>Theme: System</ThemedText>
-      <ThemedText onPress={() => setThemeMode("light")}>Theme: Light</ThemedText>
-      <ThemedText onPress={() => setThemeMode("dark")}>Theme: Dark</ThemedText>
-    </ThemedView>
-  )
+  function handleUserDisplay(){
+    switch(accountType){
+        case "guest":
+          return(
+            <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+              <ThemedText>Settings Screen</ThemedText>
+              <ThemedText>accountType: {accountType}</ThemedText>
+              <Button onPress={() => router.push("/login")}>Log In</Button>
+            </ThemedView>
+          );
+        default:
+          return(
+            <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+              <ThemedText>Settings Screen</ThemedText>
+              <ThemedText>accountType: {accountType}</ThemedText>
+              <ThemedText>themeMode: {themeMode}</ThemedText>
+              <ThemedText onPress={() => setThemeMode("system")}>Theme: System</ThemedText>
+              <ThemedText onPress={() => setThemeMode("light")}>Theme: Light</ThemedText>
+              <ThemedText onPress={() => setThemeMode("dark")}>Theme: Dark</ThemedText>
+              <Button onPress={async () => {
+                  await logout(); 
+                  router.replace("/(tabs)")
+                }}
+              >Log Out</Button>
+            </ThemedView>
+          );
+      }
+  }
+  return handleUserDisplay()
 }
