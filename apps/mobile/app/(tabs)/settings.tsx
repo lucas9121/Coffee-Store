4c45857d9c92c992c1e6427593a99a0fad1c929c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-10 12:48:31 -0400

    adding login button

--- apps/mobile/app/(tabs)/settings.tsx
+++ apps/mobile/app/(tabs)/settings.tsx
@@ -2,16 +2,20 @@ import { ThemedView } from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
 import { useAuth } from "@/context/AuthContext";
 import { useThemeMode } from "@/context/ThemeContext";
+import { Button } from "@react-navigation/elements";
+import { useRouter } from "expo-router";
 
 
 
 export default function SettingsScreen() {
   const { themeMode, setThemeMode } = useThemeMode();
   const {accountType, setAccountType} = useAuth();
+  const router = useRouter();
 
   return(
     <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
       <ThemedText>Settings Screen</ThemedText>
+      <Button onPress={() => router.push("/(auth)/login")}>Log In</Button>
       <ThemedText>accountType: {accountType}</ThemedText>
       <ThemedText onPress={() => setAccountType("guest")} >Set Guest</ThemedText>
       <ThemedText onPress={() => setAccountType("user")} >Set User</ThemedText>
