05bf757d0752602686596a5c99ffa9b0647dd668
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-10 14:58:16 -0400

    adding themed text input

---
+++ apps/mobile/components/themed-text-input.tsx
@@ -0,0 +1,69 @@
+import { TextInput, StyleSheet, type TextInputProps } from 'react-native';
+import { useThemeColor } from '@/hooks/use-theme-color';
+
+export type ThemedTextInputProps = TextInputProps & {
+  lightColor?: string;
+  darkColor?: string;
+  lightBackgroundColor?: string;
+  darkBackgroundColor?: string;
+  lightBorderColor?: string;
+  darkBorderColor?: string;
+  lightPlaceholderColor?: string;
+  darkPlaceholderColor?: string;
+};
+
+export function ThemedTextInput({ 
+  style, 
+  lightColor, 
+  darkColor, 
+  lightBackgroundColor,
+  darkBackgroundColor,
+  lightBorderColor,
+  darkBorderColor,
+  lightPlaceholderColor,
+  darkPlaceholderColor,
+  ...otherProps 
+}: ThemedTextInputProps) {
+  const textColor = useThemeColor(
+    { light: lightColor, dark: darkColor }, 
+    "text"
+  );
+
+  const backgroundColor = useThemeColor(
+    {light: lightBackgroundColor, dark: darkBackgroundColor}, 
+    "background"
+  );
+
+  const borderColor = useThemeColor(
+    {light: lightBorderColor, dark: darkBorderColor},
+    "text"
+  );
+
+  const placeholderTextColor = useThemeColor(
+    {light: lightPlaceholderColor, dark: darkPlaceholderColor},
+    "text"
+  );
+
+  return (
+    <TextInput style={[
+      styles.input,
+      { 
+        color: textColor,
+        backgroundColor,
+        borderColor
+      },
+      style
+    ]} 
+    placeholderTextColor={placeholderTextColor}
+    {...otherProps} />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    padding: 12,
+    borderRadius: 8,
+    width: "100%"
+  }
+})
