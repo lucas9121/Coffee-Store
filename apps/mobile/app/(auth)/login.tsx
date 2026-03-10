9828959d3c609c15f2ce7807ccac5c5be13a25f2
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-10 12:57:28 -0400

    updating log in

--- apps/mobile/app/(auth)/login.tsx
+++ apps/mobile/app/(auth)/login.tsx
@@ -1,10 +1,29 @@
 import {ThemedView} from "@/components/themed-view";
 import { ThemedText } from "@/components/themed-text";
+import { Button } from "@react-navigation/elements";
+import { useRouter } from "expo-router";
+import { useAuth } from "@/context/AuthContext";
+
 
 export default function LoginScreen() {
+  const router = useRouter();
+  const { login } = useAuth();
+
+  async function handleUserLogin() {
+    await login("mock-accesss-teoken-user", "mock-refresh-token-user", "user");
+    router.replace("/(tabs)");
+  };
+
+  async function handleWorkerLogin() {
+    await login("mock-accesss-teoken-user", "mock-refresh-token-user", "worker");
+    router.replace("/(tabs)");
+  };
+
   return(
     <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}} >
       <ThemedText>Login Screen</ThemedText>
+      <Button onPress={handleUserLogin}>Log In as User</Button>
+      <Button onPress={handleWorkerLogin}>Log In as Worker</Button>
     </ThemedView>
   )
 }
