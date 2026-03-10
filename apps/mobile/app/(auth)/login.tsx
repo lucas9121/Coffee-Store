85355019f3a9613d70d19da829c5c80caa78a2ba
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-10 14:22:33 -0400

    updating log in screen

--- apps/mobile/app/(auth)/login.tsx
+++ apps/mobile/app/(auth)/login.tsx
@@ -1,29 +1,73 @@
-import {ThemedView} from "@/components/themed-view";
-import { ThemedText } from "@/components/themed-text";
+import { useState } from "react";
+import { TextInput, StyleSheet } from "react-native";
 import { Button } from "@react-navigation/elements";
 import { useRouter } from "expo-router";
+
+import {ThemedView} from "@/components/themed-view";
+import { ThemedText } from "@/components/themed-text";
 import { useAuth } from "@/context/AuthContext";
 
 
 export default function LoginScreen() {
   const router = useRouter();
   const { login } = useAuth();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  async function handleSubmit(): Promise<void> {
+    console.log("Login form submitted: ", {email, password})
+  }
 
   async function handleUserLogin() {
-    await login("mock-accesss-teoken-user", "mock-refresh-token-user", "user");
-    router.replace("/(tabs)");
+    await login("mock-access-teoken-user", "mock-refresh-token-user", "user");
+    router.replace("/");
   };
 
   async function handleWorkerLogin() {
-    await login("mock-accesss-teoken-user", "mock-refresh-token-user", "worker");
-    router.replace("/(tabs)");
+    await login("mock-access-teoken-user", "mock-refresh-token-user", "worker");
+    router.replace("/");
   };
 
   return(
-    <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}} >
-      <ThemedText>Login Screen</ThemedText>
+    <ThemedView style={styles.view} >
+      <ThemedText type="title" >Login Screen</ThemedText>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+        style={styles.formText}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.formText}
+      />
+
+      <Button onPress={handleSubmit}>Log in</Button>
+
+      <ThemedText type="subtitle">Dev shortcuts</ThemedText>
       <Button onPress={handleUserLogin}>Log In as User</Button>
       <Button onPress={handleWorkerLogin}>Log In as Worker</Button>
     </ThemedView>
   )
 }
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1, 
+    alignItems: "center", 
+    justifyContent: "center",
+    padding: 24,
+    gap: 12,
+  },
+  formText: {
+    borderWidth: 1,
+    borderColor: "#999",
+    padding: 12,
+    borderRadius: 8,
+  }
+})
