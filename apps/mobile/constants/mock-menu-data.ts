3b8a6d723da0479619ee7171d8d5e1ac11cfa175
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-15 21:09:40 -0400

    moving mock data

---
+++ apps/mobile/constants/mock-menu-data.ts
@@ -0,0 +1,87 @@
+export const favoriteItems = [
+  { id: "1", name: "Latte", price: 4, image: require("@/assets/images/church-kiosk-temp-logo.png")},
+  { id: "2", name: "Mocha", price: 4, image: require("@/assets/images/church-kiosk-temp-logo.png")},
+  { id: "3", name: "Cappuccino", price: 4, image: require("@/assets/images/church-kiosk-temp-logo.png")},
+];
+
+export const recentItems = [
+  { id: "1", name: "Iced Coffee", price: 4, image: require("@/assets/images/church-kiosk-temp-logo.png")},
+  { id: "2", name: "Blueberry Muffin", price: 4, image: require("@/assets/images/church-kiosk-temp-logo.png")},
+  { id: "3", name: "Orange Juice", price: 4, image: require("@/assets/images/church-kiosk-temp-logo.png")},
+]
+
+export const menuItems = [
+  {
+    id: "coffee-cappuccino",
+    name: "Cappuccino",
+    category: "coffee",
+    price: 4.5,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+  {
+    id: "coffee-latte",
+    name: "Latte",
+    category: "coffee",
+    price: 4.5,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+  {
+    id: "coffee-espresso",
+    name: "Espresso",
+    category: "coffee",
+    price: 3,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+  {
+    id: "coffee-iced",
+    name: "Iced Coffe",
+    category: "coffee",
+    price: 3,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+
+  {
+    id: "juice-orange",
+    name: "Orange Juice",
+    category: "juice",
+    price: 3.5,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+
+  {
+    id: "food-pao-de-queijo",
+    name: "Pão de Queijo",
+    category: "food",
+    price: 3,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+  {
+    id: "food-misto-quente",
+    name: "Misto Quente",
+    category: "food",
+    price: 4,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+
+  {
+    id: "dessert-brownie",
+    name: "Brownie",
+    category: "desserts",
+    price: 3,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+  {
+    id: "dessert-lemon-cake",
+    name: "Lemon Cake",
+    category: "desserts",
+    price: 3,
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+  {
+    id: "dessert-carrot-cake",
+    name: "Carrot Cake",
+    price: 3,
+    category: "desserts",
+    image: require("@/assets/images/church-kiosk-temp-logo.png"),
+  },
+];
