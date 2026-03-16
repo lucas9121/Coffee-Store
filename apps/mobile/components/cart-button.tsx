f7e1b9fe30d99b12cab4ca8f83aedf2e108f5419
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-16 13:46:33 -0400

    adding router

--- apps/mobile/components/cart-button.tsx
+++ apps/mobile/components/cart-button.tsx
@@ -1,15 +1,22 @@
 import { Pressable, StyleSheet } from "react-native";
 import { ThemedText } from "./ui/themed-text";
 import { useCart } from "@/context/CartContext";
+import { useRouter } from "expo-router";
 
 
 export function CartButton(){
   const {cartItems} = useCart();
+  const router = useRouter();
 
   const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
 
   if(cartCount > 0){
-    return <Pressable style={styles.cartButton}><ThemedText>🛒 {cartCount.toString()}</ThemedText></Pressable>
+    return <Pressable 
+      style={styles.cartButton}
+      onPress={() => router.push("/modal")}
+    >
+      <ThemedText>🛒 {cartCount.toString()}</ThemedText>
+    </Pressable>
   }
 
   return null;
