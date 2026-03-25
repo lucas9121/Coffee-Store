ac5f73dd06f410cf45b7095456e339bb3702d407
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-25 14:33:24 -0400

    adding backend login

--- apps/mobile/app/(auth)/login.tsx
+++ apps/mobile/app/(auth)/login.tsx
@@ -7,6 +7,7 @@ import { ThemedView } from "@/components/ui/themed-view";
 import { ThemedText } from "@/components/ui/themed-text";
 import { ThemedTextInput } from "@/components/ui/themed-text-input";
 import { useAuth } from "@/context/AuthContext";
+import { loginUser } from "@/services/auth-api";
 
 
 export default function LoginScreen() {
@@ -15,18 +16,38 @@ export default function LoginScreen() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [loginError, setLoginError] = useState("");
 
   const params = useLocalSearchParams<{ fromCheckout?: string }>();
   const fromCheckout = params.fromCheckout === "true";
 
+
   async function handleSubmit(): Promise<void> {
-    console.log("Login form submitted: ", {email, password})
-    if (fromCheckout) {
-      router.replace("/cart");
-    } else {
-      router.replace("/");
+    setLoginError("");
+    setIsSubmitting(true);
+
+    try {
+      const data = await loginUser({ email, password });
+
+      const accessToken = data.token;
+      const refreshToken = data.refreshToken ?? null;
+      const accountType = data.user.account;
+
+      await login(accessToken, refreshToken, accountType);
+
+      if (fromCheckout) {
+        router.replace("/cart");
+      } else {
+        router.replace("/");
+      }
+    } catch (error) {
+      setLoginError("Login failed. Please check your email and password.");
+      console.error(error);
+    } finally {
+      setIsSubmitting(false);
     }
-  }
+  };
 
   async function handleUserLogin() {
     await login("mock-access-token-user", "mock-refresh-token-user", "user");
@@ -64,7 +85,11 @@ export default function LoginScreen() {
         }
       />
 
-      <Button onPress={handleSubmit}>Log in</Button>
+      <Button onPress={handleSubmit}>
+        {isSubmitting ? "Logging in..." : "Log in"}
+      </Button>
+
+      {loginError ? <ThemedText style={styles.errorText}>{loginError}</ThemedText> : null}
 
       {fromCheckout && (
         <Button onPress={() => router.push("/guest-checkout")}>
@@ -91,4 +116,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 10,
   },
+  errorText:{
+    color: "red",
+  }
 });
