abe0001a832773d2692030425be66eb144ab367f
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-03-25 14:07:09 -0400

    updating worker orders

--- apps/mobile/components/worker-orders-content.tsx
+++ apps/mobile/components/worker-orders-content.tsx
@@ -3,9 +3,59 @@ import { StyleSheet } from "react-native";
 import { ThemedScrollView } from "./ui/themed-scroll-view";
 import { ThemedText } from "./ui/themed-text";
 import { Section } from "./section";
+import { getAllOrders } from "@/services/orders-api";
 
+type OrderItem = {
+  item: string;
+  quantity: number;
+  priceAtPurchase: number;
+};
 
-export function WorkerOrdersContent() {
+type Order = {
+  id: string;
+  customerName: string;
+  status: string;
+  orderItems: OrderItem[];
+  totalPrice: number;
+  timestamp: string;
+};
+
+type MenuItem = {
+  id: string;
+  name: string;
+  price: number;
+  image: string | number;
+  category: string;
+  inStock: boolean;
+};
+
+type WorkerOrdersContentProps = {
+  accountType: string;
+  borderColor: string;
+  token: string | null;
+};
+
+export function WorkerOrdersContent({
+  accountType,
+  borderColor,
+  token
+}: WorkerOrdersContentProps) {
+  const [orders, setOrders] = useState<Order[]>([])
+  const [placed, setPlaced] = useState<MenuItem[]>([])
+
+  async function loadOrders(): Promise<void>{
+    try {
+      const data = await getAllOrders(token);
+      setOrders(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() =>{
+    loadOrders();
+  }, [])
+
   return (
     <ThemedScrollView contentContainerStyle={styles.screenContent}>
       <ThemedText type="title">Worker Orders</ThemedText>
@@ -18,7 +68,7 @@ export function WorkerOrdersContent() {
         <ThemedText>Walk-up orders will appear here</ThemedText>
       </Section> */}
 
-            <Section title="Placed">
+      <Section title="Placed">
         <ThemedText>No placed orders yet</ThemedText>
       </Section>
 
