03058b0fffcf0252ae9887fb7d19b9265d892bb5
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-04-10 18:56:04 -0400

    fixing url

--- apps/mobile/services/store-settings.ts
+++ apps/mobile/services/store-settings.ts
@@ -1,9 +1,9 @@
 import { sendRequest } from "./api";
 
-const BASE_URL = `${process.env.EXPO_PUBLIC_API_URL}/store/status`
+const BASE_URL = `${process.env.EXPO_PUBLIC_API_URL}/store`
 
 export async function getStoreStatus(){
-  return await sendRequest(BASE_URL)
+  return await sendRequest(`${BASE_URL}/status`)
 }
 
 export async function setStoreOverride(
