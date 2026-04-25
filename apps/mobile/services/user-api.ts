1d1d6e23686f766b5bae33c67624f8d5ffb2da9c
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-04-25 12:38:56 -0400

    adding update user profile function

--- apps/mobile/services/user-api.ts
+++ apps/mobile/services/user-api.ts
@@ -5,3 +5,14 @@ const BASE_URL = `${process.env.EXPO_PUBLIC_API_URL}/users/me`;
 export async function getCurrentUser(token: string | null){
   return await sendRequest(BASE_URL, "GET", null, token)
 };
+
+export async function updateUserProfile(
+  payload: {
+    name?: string;
+    email?: string;
+    password: string;
+  },
+  token: string | null
+) {
+  return await sendRequest(`${BASE_URL}/profile`, "PATCH", payload, token)
+}
