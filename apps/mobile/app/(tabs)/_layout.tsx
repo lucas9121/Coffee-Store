4d2396e8a088ecf443483fd0f72ed804143b1dd4
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-05-30 16:51:18 -0400

    adding icons

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { useAuth } from "../../context/AuthContext"
+import { Ionicons } from "@expo/vector-icons";
 
 
 export default function TabLayout() {
@@ -13,19 +14,28 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home'
+          title: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          )
         }}
       />
       <Tabs.Screen
         name="orders"
         options={{
-          title: accountType === "worker" ? "Orders" : "Order"
+          title: accountType === "worker" ? "Orders" : "Order",
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name={accountType === "worker" ? "clipboard-outline" : 'cafe-outline'} size={size} color={color} />
+          )
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: "Settings"
+          title: "Settings",
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name='settings-outline' size={size} color={color} />
+          )
         }} 
       />
     </Tabs>
