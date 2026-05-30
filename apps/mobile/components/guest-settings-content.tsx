bb329cfc601b0b7db3460e2e30a4e6090eb5d9f2
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-05-30 15:55:55 -0400

    updating guest settings

--- apps/mobile/components/guest-settings-content.tsx
+++ apps/mobile/components/guest-settings-content.tsx
@@ -1,16 +1,11 @@
-import { Button } from "@react-navigation/elements";
-import { ThemedView } from "./ui/themed-view";
-import { ThemedText } from "./ui/themed-text";
-import { useRouter } from "expo-router";
+import LoginForm from "./login-form";
 
 
-export function GuestSettingsContent(){
-  const router = useRouter();
-    return(
-      <ThemedView style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-        {/* <ThemedText type="title" >Settings Screen</ThemedText> */}
-        <Button onPress={() => router.push("/login")}>Log In</Button>
-        <Button onPress={() => router.push("/signup")}>Sign up</Button>
-      </ThemedView>
-    );
+export function GuestSettingsContent() {
+  return (
+    <LoginForm
+      title="Log In"
+      showSignupButton
+    />
+  );
 }
