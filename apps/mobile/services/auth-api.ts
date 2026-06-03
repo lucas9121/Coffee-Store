ee53a87c4860a53e3cf9b6b65d7dea5adad6ccd3
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-06-03 18:14:39 -0400

    adding password reset functions

--- apps/mobile/services/auth-api.ts
+++ apps/mobile/services/auth-api.ts
@@ -29,3 +29,15 @@ export async function signupUser(payload: SignupPayload) {
 export async function logoutUser(token: string | null){
   return await sendRequest(`${BASE_URL}/me/logout`, "POST", null, token)
 }
+
+export async function getForgotPasswordQuestions(payload: { email: string }) {
+  return await sendRequest(`${BASE_URL}/forgot-password/questions`, "POST", payload, null);
+}
+
+export async function resetForgotPassword(payload: {
+  email: string;
+  answers: string[];
+  newPassword: string;
+}) {
+  return await sendRequest(`${BASE_URL}/forgot-password/reset`, "POST", payload, null);
+}
