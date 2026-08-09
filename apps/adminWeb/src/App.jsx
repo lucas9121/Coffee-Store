5c5a8d8d8b3841820d3026ccb61836b2a18ab17a
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-08-08 20:46:33 -0400

    adding login page

--- apps/adminWeb/src/App.jsx
+++ apps/adminWeb/src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>ChurchKiosk Admin</h1>} />
-      <Route path="/login" element={<h1>Admin Login</h1>} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
