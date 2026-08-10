76d965a806c77ddd5f25bec70e9ecef87607772d
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-08-10 19:34:28 -0400

    adding Layout

--- apps/adminWeb/src/App.jsx
+++ apps/adminWeb/src/App.jsx
@@ -2,19 +2,21 @@ import { Route, Routes } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import DashboardPage from "./pages/DashboardPage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route 
-        path="/" 
         element={
           <ProtectedRoute>
-            <DashboardPage />
+            <Layout />
           </ProtectedRoute>
         } 
-      />
+      >
+        <Route path="/" element={<DashboardPage />} />
+      </Route>
     </Routes>
   );
 }
