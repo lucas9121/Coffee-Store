65bdfc839caa5ae4ca08514b09c3a4bdecb14b81
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-08-18 17:36:51 -0400

    adding new pages

--- apps/adminWeb/src/App.jsx
+++ apps/adminWeb/src/App.jsx
@@ -2,6 +2,10 @@ import { Route, Routes } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import DashboardPage from "./pages/DashboardPage";
+import MenuPage from "./pages/MenuPage";
+import SchedulePage from "./pages/SchedulePage";
+import UsersPage from "./pages/UsersPage";
+import SettingsPage from "./pages/SettingsPage";
 import Layout from "./components/Layout";
 
 function App() {
@@ -16,6 +20,10 @@ function App() {
         } 
       >
         <Route path="/" element={<DashboardPage />} />
+        <Route path="/menu" element={<MenuPage />}/>
+        <Route path="/users" element={<UsersPage />}/>
+        <Route path="/schedule" element={<SchedulePage />}/>
+        <Route path="/settings" element={<SettingsPage />}/>
       </Route>
     </Routes>
   );
