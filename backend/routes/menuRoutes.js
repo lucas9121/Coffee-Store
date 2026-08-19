2cab6a2d9aa83e2edc45ee24d9081c9203a19533
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-08-18 21:25:10 -0400

    updating route authorization

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const requireAuth = require("../middleware/requireAuth");
+const authorizeRoles = require("../middleware/authorizeRoles");
 const {getOrderItem, getAllOrderItems, createItem, updateOrderItem, deleteOrderItem} = require("../controllers/orderItemController")
 
-router.post("/", createItem);
 router.get("/", getAllOrderItems);
 router.get("/:id", getOrderItem);
-router.patch("/:id", updateOrderItem);
-router.delete("/:id", deleteOrderItem);
+router.post("/", requireAuth, authorizeRoles(["admin"]), createItem);
+router.patch("/:id", requireAuth, authorizeRoles(["admin"]), updateOrderItem);
+router.delete("/:id", requireAuth, authorizeRoles(["admin"]), deleteOrderItem);
 
 module.exports = router
