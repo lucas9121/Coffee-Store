59b5ad5d02afa207ff36277221b95c3057d29a6d
Lucas Carlos <92693783+lucas9121@users.noreply.github.com>
2026-09-03 14:34:51 -0400

    updating menu page

--- apps/adminWeb/src/pages/MenuPage.jsx
+++ apps/adminWeb/src/pages/MenuPage.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
+import { getMenuItems } from "../services/menu-api";
 function MenuPage() {
+  const [menuItems, setMenuItems] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect( () => {
+    (async () => {
+      try {
+        setLoading(true)
+        const data = await getMenuItems()
+        setMenuItems(data)
+        setError("")
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setLoading(false)
+      }
+    })()
+  }, [])
+  console.log(menuItems)
+
   return (
     <main>
       <h1>Menu</h1>
-      <p>Manage Catedral Café menu items.</p>
+      {error && <p>{error}</p>}
+      {
+        loading ?
+        <p>loading...</p> :
+        menuItems.map((item) => {
+          return(
+            <div key={item._id}>
+              <img src={item.image || "/images/logo.jpg" } alt={item.name} />
+              <p className="menuItem">{item.name} </p>
+              <p className="menuItem">{item.category} </p>
+              <p className="menuItem">${item.price.toFixed(2)} </p>
+              <p className="menuItem">{item.inStock ? "In Stock" : "Out of Stock"} </p>
+              <button type="button" className="menuAction">Edit </button>
+              <button type="button" className="menuAction">Delete </button>
+            </div>
+          )
+        })
+      }
     </main>
   );
 }
